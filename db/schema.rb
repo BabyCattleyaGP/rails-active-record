babd357492f3a9f5add7dca54c8969c50aea0acf
BabyCattleyaGP <baby16001@mail.unpad.ac.id>
2020-04-13 17:28:53 +0700

    add new-model : category

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_13_100425) do
+ActiveRecord::Schema.define(version: 2020_04_13_102501) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "name"
